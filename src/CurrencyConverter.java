2411a3e668f0d1d9dc99f42ad14f928cfda720f9
beril314 <berildermenci@gmail.com>
2024-10-06 11:44:37 +0100

    second commit

--- src/CurrencyConverter.java
+++ src/CurrencyConverter.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+//Scanner is imported so we can read inputs from user
+
 public class CurrencyConverter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //Options are displayed to the user
         System.out.println("Choose one from the following actions");
         System.out.println("type '0' for GBP to USD");
         System.out.println("type '1' for GBP to EUR");
@@ -10,10 +13,15 @@ public class CurrencyConverter {
         System.out.println("type '4' for USD to GBP");
         System.out.println("type '5' for USD to EUR");
 
+        //Take the user's choice as input, store the value in the 'choice' variable
         int choice = scanner.nextInt();
+
+        //After the user selects the type of conversion, they are asked to input the amount they want to convert.
+        // This amount is stored in the inputAmount variable.
         System.out.println("Enter the amount you would like to convert:");
         int inputAmount = scanner.nextInt();
 
+        //Execute the correct conversion based on the users choice.
         switch (choice) {
             case 0:
                 System.out.println("£" +inputAmount + " = " +"$" +inputAmount *1.31);
